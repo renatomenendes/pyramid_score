9ed815be51f310eacc075cbcb9997a28cbf94571
renatomenendes <89153259+renatomenendes@users.noreply.github.com>
2024-09-10 13:04:45 -0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name='rfm_pyramid_score',
+    version='0.1',
+    description='Análise de clientes com RFM Pyramid Score',
+    author='Renato Cesar Menendes Cruz',
+    author_email='renatomenendes@yahoo.com.br',
+    packages=find_packages(),
+    install_requires=[
+        'pandas>=1.0',
+        'numpy>=1.18',
+        'matplotlib>=3.0'
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License'
+    ],
+)
